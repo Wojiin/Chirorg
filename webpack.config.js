f9dd6c48175cce92b29b57bfb099dba66bce7c2b
Quentin Maia <q.maia@laposte.net>
2025-12-19 18:56:44 +0100

    sync

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,9 @@ Encore
     // enables the Symfony UX Stimulus bridge (used in assets/stimulus_bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
+    // enables the Symfony UX Stimulus bridge (used in assets/stimulus_bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
+
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
